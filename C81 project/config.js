2a498da593948447ff895bb7625e560a085a5e07
꧁༒☬Ðєαтн☬༒꧂ <88191864+BadBlackBrine@users.noreply.github.com>
2022-08-29 18:34:26 +0530

    Add files via upload

---
+++ C81 project/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBejwVNN2h1X4jjGPG_oYgZrC0poe42nXs",
+    authDomain: "satori-ap.firebaseapp.com",
+    databaseURL: "https://satori-ap-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "satori-ap",
+    storageBucket: "satori-ap.appspot.com",
+    messagingSenderId: "28359015168",
+    appId: "1:28359015168:web:b1802be7ec9794610291e5"
+  };
